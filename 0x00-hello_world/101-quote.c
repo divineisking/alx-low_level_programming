343966e879d1571c3c601ac68e36c728402be70f
divineisking <josefdivine162@gmail.com>
2022-07-03 03:48:38 -0700

    created 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/*
+ * main- is a function, it prints the
+ * sentence in the printf function
+ * return- is a function, returns a value (1)when the ckde runs well
+ */
+int main(void)
+{
+	write(2, " and that piece of art
+			is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
