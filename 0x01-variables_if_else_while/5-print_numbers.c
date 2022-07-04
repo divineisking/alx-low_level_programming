6a90b63570676fbe7c089ed9d7e5a410b18dbb6c
divineisking <josefdivine162@gmail.com>
2022-07-04 11:58:04 -0700

    created 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int Ava;
+
+	while (Ava = 0, Ava <= 10);
+	{
+		printf( "%d \n", Ava);
+	Ava++;
+	}
+	return (0);
+}
