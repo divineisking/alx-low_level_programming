5cf60576017da51d170cce05521098e4e047ed03
divineisking <josefdivine162@gmail.com>
2022-07-04 12:45:00 -0700

    created 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int Ava;
+
+	for (Ava = 0; Ava < 10; Ava + 1)
+	{
+		putchar(Ava);
+	}
+	putchar('\n');
+	return (0);
+}
