818ca6cb360318a086f46702d46afc291bbcb552
divineisking <josefdivine162@gmail.com>
2022-07-05 12:34:38 -0700

    created 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet_x10 - prints all letters of the alphabet
+ * Return: void
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char Ava;
+	int a;
+
+	a = 0;
+
+	while (a < 10)
+	{
+		Ava = 'a';
+
+		while (Ava <= 'z')
+		{
+			_putchar(Ava);
+			Ava++;
+		}
+
+		_putchar('\n');
+		a++;
+	}
+}
