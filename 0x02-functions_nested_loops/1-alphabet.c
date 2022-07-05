3262731a31f585a3cc2476994918a7fb8a54177c
divineisking <josefdivine162@gmail.com>
2022-07-05 09:56:13 -0700

    creahed 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet - prints letters a+z in lowercase
+ *
+ * Return: void 
+ *
+ */
+
+int main(void)
+{
+	return (0)
+}
+
+void print_alphabet(void)
+{
+	char Ava;
+
+	Ava = 'a';
+
+	while (Ava <= z)
+	{
+		_putchar(Ava);
+		Ava++;
+	}
+	_putchar('\n');
+}
