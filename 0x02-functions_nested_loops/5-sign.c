d4673d83db326ae504416104b72b500478872f16
divineisking <josefdivine162@gmail.com>
2022-07-05 14:11:06 -0700

    created 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+int print_sign(int Ava)
+{
+	if (Ava > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if ( Ava == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (Ava < 0)
+	{
+		_putchar('-')
+	}
+	return (-1)
+}
