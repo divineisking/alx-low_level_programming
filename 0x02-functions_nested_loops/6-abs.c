1ca9f03f605c3df8e52e926f638393fad15b9587
divineisking <josefdivine162@gmail.com>
2022-07-05 14:33:30 -0700

    created 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absoukt value of an int
+ * @Ava: int to check
+ * Return: return value of _abs
+ */
+
+int _abs(int Ava)
+{
+	if (Ava >= 0)
+	{
+		return (Ava);
+	}
+
+	return (-Ava);
+}
