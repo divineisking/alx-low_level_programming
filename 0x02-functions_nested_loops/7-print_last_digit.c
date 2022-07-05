94f94dd8f82bad78052ed7068cf074f449033da6
divineisking <josefdivine162@gmail.com>
2022-07-05 14:51:36 -0700

    createx 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the kast digit of a number 
+ * @Ava: number to check
+ * Return: returns 
+ */
+
+int print_last_digit(int Ava)
+{
+	int a;
+
+	if (Ava < 0)
+	{
+		Ava = -Ava;
+	}
+
+	a = Ava % 10;
+	
+	if (a < )
+	{
+		a = -a;
+	}
+
+	_putchar(a + '0');
+	return (Ava);
+}
