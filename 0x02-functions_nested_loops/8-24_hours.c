1bed283ee3c42ed74b50459960d3d6f0464188dd
divineisking <josefdivine162@gmail.com>
2022-07-05 15:42:07 -0700

    created 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+	int a, c;
+
+	a = 0;
+
+	while (a < 24)
+	{
+		c = 0;
+		while (c < 60)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+			_putchar('\n');
+			c++;
+		}
+		a++;
+	}
+}
