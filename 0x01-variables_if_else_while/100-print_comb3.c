f5857fde85fcdf160a9df688a74dd880a39f812d
divineisking <josefdivine162@gmail.com>
2022-07-05 05:28:59 -0700

    created 101

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int Ava, Afa;
+
+	for (Ava = 48; Ava <= 56; Ava++)
+	{
+		for (Afa = 49; Afa <= 57; Afa++)
+			if (Afa > Ava)
+			{
+				putchar(Ava);
+				putchar(Afa);
+				if (Ava != 56 || Afa != 57)
+				{
+					putchar(' ');
+					putchar(',');
+				}
+			}
+	}
+	putchar('\n');
+	return (0);
+}
