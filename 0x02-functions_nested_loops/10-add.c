852f357b89e2e7104619d435905354a41b4b2d0d
divineisking <josefdivine162@gmail.com>
2022-07-06 02:54:39 -0700

    created 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two variables and returns their sum
+ * @int: values type
+ * Return:
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	int c = a + b;
+
+	return (c);
+}
