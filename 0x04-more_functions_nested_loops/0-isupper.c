877b4765606caa76daf23f7f88f52a002ba0e898
divineisking <josefdivine162@gmail.com>
2022-07-07 17:42:28 -0700

    updated 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
+/**
+ * _isupper - checks if a anf alphabet is uppercase
+ * @a: value to check
+ * Return: returns 0 or 1
+ */
+
 int _isupper(int a)
 {
-	return ( a >= 'A' && a <= 'Z');
+	return (a >= 'A' && a <= 'Z');
 }
