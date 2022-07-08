ba3a59f2cbccec37f0876d0035a28d711a0bab00
divineisking <josefdivine162@gmail.com>
2022-07-07 18:01:47 -0700

    created 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multipiles 2 integers
+ * @a: integer to multiply
+ * @b: integer to multiply by
+ * Return: 0 if successful
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
