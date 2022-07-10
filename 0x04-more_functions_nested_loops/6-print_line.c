91a7ca84c3d4a97363dffac87c9818b548e2b303
divineisking <josefdivine162@gmail.com>
2022-07-10 10:16:29 -0700

    created 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: number of times to print _
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		i++
+	}
+
+	_putchar('\n');
+}
