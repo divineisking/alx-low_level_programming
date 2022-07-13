9fd317eecf3a03eed57838f63f97dd7844d94739
divineisking <josefdivine162@gmail.com>
2022-07-13 06:09:24 -0700

    created 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _put - returns or prints a tring standard output
+ * @str: pointer to string to print
+ */
+
+void _put(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+
+	_putchar('\n');
+}
