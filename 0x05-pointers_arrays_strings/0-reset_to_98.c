7ae327200f09b294e1d4200a03e66e9fef4b4d4f
divineisking <josefdivine162@gmail.com>
2022-07-13 02:04:13 -0700

    created readme, 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of the variable th
+ * pointer points to, to 98
+ * @n: the pointer to the variable to reset/update
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+This dir contains all projects relating to concepts like pointers, arrays, strings and memory structures
+
+This readme file contains the description of what each function in this dir does.
