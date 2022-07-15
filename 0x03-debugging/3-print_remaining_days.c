54181f2de95fd1ca6582601136fae3faf99507c7
divineisking <josefdivine162@gmail.com>
2022-07-15 08:59:01 -0700

    corrected 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,7 +22,7 @@ void print_remaining_days(int month, int day, int year)
 
 		printf("Day of the year: %d\n", day);
 
-		printf("There are: %d\n days left" 366 - day);
+		printf("There are: %d\n days left", 366 - day);
 	}
 
 	else
