846df7805e6ac2c6480fba96e956ce70a5923635
divineisking <josefdivine162@gmail.com>
2022-07-14 21:40:59 -0700

    created 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * string_toupper - changes a string to uppercase
+ * @s: string to change
+ * Return: the resulting string
+ *
+ *
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+
+	return (s);
+}
