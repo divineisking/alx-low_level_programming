c42eade209ca76af09e1ba31e99402ded785c35c
divineisking <josefdivine162@gmail.com>
2022-08-03 10:04:09 -0700

    corrected 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -20,16 +20,12 @@ int _atoi(char *s)
 	digit = 0;
 
 	while (s[len] != '\0')
-	{
 		len++;
-	}
 
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-		{
 			++d;
-		}
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
@@ -38,7 +34,6 @@ int _atoi(char *s)
 			{
 				digit = -digit;
 			}
-
 			n = n * 10 + digit;
 			f = 1;
 
@@ -50,12 +45,10 @@ int _atoi(char *s)
 		}
 		i++;
 	}
-
 	if (f == 0)
 	{
 		return (0);
 	}
-
 	return (n);
 }
 
